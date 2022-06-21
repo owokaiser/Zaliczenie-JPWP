6321c5347459240d15cab2b63edba9bed63abfa2
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-06-21 10:41:10 +0000

    Finish

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+    int liczba=scan.nextInt();
+    int suma=0;
+    System.out.println("Podaj liczby różne od 5: ");
+    while(liczba!=5)
+      {
+        if(liczba%2!=0)
+        {
+          suma=suma+liczba;
+        }
+        liczba=scan.nextInt();
+      }
+    System.out.println("Suma liczb nieparzystych wynosi: "+suma);
   }
 }
